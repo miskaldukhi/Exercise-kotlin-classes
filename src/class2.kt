2f47e1a03bb541d9e0319a377bf93ba47eaf6153
miskaldukhi <miskaldukhi@gmail.com>
2025-02-16 18:52:39 +0300

    class 2

---
+++ src/class2.kt
@@ -0,0 +1,13 @@
+
+fun main(){
+
+
+}
+class Books constructor(var title: String ="The Poppy War",
+                       val author:String ="R.F.Kuang",
+                       val yearPublished: Int = 2018){
+    fun getBookInfo(){
+        var
+    }
+
+}
