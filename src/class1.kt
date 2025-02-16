5ba2a11db10ccdb0387583c0c07812bae5f3d2e0
miskaldukhi <miskaldukhi@gmail.com>
2025-02-16 17:38:41 +0300

    part 1 done

---
+++ src/class1.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var thePoppyWar = Book()
+    thePoppyWar.title ="The Poppy War"
+    thePoppyWar.author= "R.F.Kuang"
+    thePoppyWar.yearPublished= 2018
+
+}
+class Book {
+    var title: String = ""
+    var author: String = ""
+    var yearPublished: Int = 2018
+}
